307cc279497d1a2e3cfaefdd9d21ef16bd5c652d
Alexis Duran <alexis.duranm@gmail.com>
2019-10-02 18:52:45 +0200

    feat(models): add objection models declaration

---
+++ models/Action.js
@@ -0,0 +1,41 @@
+const { Model } = require("objection");
+
+class Action extends Model {
+	static get tableName() {
+		return "actions";
+	}
+
+	static get jsonSchema() {
+		return {
+			type: "object",
+			required: ["title"],
+
+			properties: {
+				id: { type: "integer" },
+				title: { type: "string", minLength: 1, maxLength: 255 },
+				description: { type: "string", minLength: 1, maxLength: 255 }
+			}
+		};
+	}
+
+	static get relationMappings() {
+		return {
+			actors: {
+				relation: Model.ManyToManyRelation,
+				modelClass: __dirname + "/Campaign",
+				join: {
+					from: "actions.id",
+					through: {
+						from: "campaigns_actions.actionId",
+						to: "campaigns_actions.campaignId"
+					},
+					to: "campaigns.id"
+				}
+			}
+		};
+	}
+}
+
+module.exports = {
+	Action
+};
---
+++ models/Campaign.js
@@ -0,0 +1,42 @@
+const { Model } = require("objection");
+const Action = require("./Action");
+
+class Campaign extends Model {
+	static get tableName() {
+		return "campaigns";
+	}
+
+	static get jsonSchema() {
+		return {
+			type: "object",
+			required: ["slug", "name"],
+
+			properties: {
+				id: { type: "integer" },
+				slug: { type: "string", minLength: 1, maxLength: 255 },
+				name: { type: "string", minLength: 1, maxLength: 255 }
+			}
+		};
+	}
+
+	static get relationMappings() {
+		return {
+			actions: {
+				relation: Model.ManyToManyRelation,
+				modelClass: Action,
+				join: {
+					from: "campaigns.id",
+					through: {
+						from: "campaigns_actions.campaignId",
+						to: "campaigns_actions.actionId"
+					},
+					to: "actions.id"
+				}
+			}
+		};
+	}
+}
+
+module.exports = {
+	Campaign
+};
