c17e4e5ed7ab0744bc0ce4fd28c2fc560c64125c
Alexis Duran <alexis.duranm@gmail.com>
2019-10-04 18:23:09 +0200

    fix(create_user): update migration to reflect campaign relation
    BREAKING CHANGE: updating older migration need to remove the database

--- migrations/20191003190523_create_users.js
+++ migrations/20191003190523_create_users.js
@@ -2,6 +2,13 @@ exports.up = function(knex) {
 	return knex.schema.createTable("users", table => {
 		table.increments("id").primary();
 		table.string("email");
+		table
+			.integer("campaignId")
+			.unsigned()
+			.references("id")
+			.inTable("campaigns")
+			.onDelete("SET NULL")
+			.index();
 	});
 };
 
