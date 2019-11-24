9715c006cafce4f9041083e209e92ed8d1418741
Orlando Del Aguila <orlando@hashlabs.com>
2019-11-23 21:21:58 -0600

    dev: change seed columns to snake_case to match database

--- seeds/002_actions.js
+++ seeds/002_actions.js
@@ -1,8 +1,8 @@
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   return knex('actions').insert([
     {
       id: 1,
-      campaignId: 1,
+      campaign_id: 1,
       title: 'Data Dues',
       description:
         'Data dues action where we request user about their debt data',
