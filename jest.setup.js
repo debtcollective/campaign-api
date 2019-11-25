00bd066c3b471b188f36dbfa34a0229eaf292aa7
Alexis Duran <alexis.duranm@gmail.com>
2019-11-25 22:44:56 +0100

    test(setup): avoid to pull env and use object literal

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,3 @@
-// Pull environment variables
-require('dotenv').config()
-
 // Database preparation
 const Knex = require('knex')
 const { Model } = require('objection')
@@ -8,3 +5,13 @@ const knexConfig = require('./knexfile')
 
 const knex = Knex(knexConfig)
 Model.knex(knex)
+
+process.env = {
+  SSO_JWT_SECRET: 'jwt-secret',
+  SSO_COOKIE_NAME: 'tdc_auth_token',
+  DEV_HOST: 'campaign-api.lvh.me',
+  DISCOURSE_LOGIN_URL: 'http://lvh.me:3000/session/sso_cookies',
+  DISCOURSE_SIGNUP_URL: 'http://lvh.me:3000/session/sso_cookies/signup',
+  INTROSPECTION: true,
+  PLAYGROUND: true
+}
