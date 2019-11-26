a38e0881430307ef1e82ddfd0536d61605bfb1ba
Orlando Del Aguila <orlando@hashlabs.com>
2019-11-26 11:29:56 -0600

    dev(data-dues): add validation with yup to resolver

--- resolvers/UserActions.js
+++ resolvers/UserActions.js
@@ -1,7 +1,66 @@
 const _ = require('lodash')
+const yup = require('yup')
 const { Action } = require('../models/Action')
 const UserAction = require('../models/UserAction')
-const validationSchema = {}
+
+// Same validations we use in the client
+// We should move these to a shared package later
+const unknown = 'Unknown'
+const debtTypes = [
+  'Student debt',
+  'Housing debt',
+  'Medical debt',
+  'Court or bail fees',
+  'Payday loans',
+  'Auto loan',
+  'Credit card debt',
+  'Other'
+]
+const studentDebtTypes = [
+  'Subsidized Stafford',
+  'Unsubsidized Stafford',
+  'Parent PLUS',
+  'Private Student loans'
+]
+const accountStatuses = [
+  'In repayment',
+  'Late on payments',
+  'Stopped payments',
+  'Sent to collections'
+]
+const phoneRegExp = /^(\([0-9]{3}\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$/
+const validationSchema = yup.object().shape({
+  fullName: yup
+    .string()
+    .min(5, 'Full name must be at least ${min} characters') // eslint-disable-line no-template-curly-in-string
+    .required('Full name is a required field'),
+  email: yup
+    .string()
+    .email('Must be a valid email')
+    .required('Email is a required field'),
+  streetAddress: yup.string(),
+  phoneNumber: yup.string().matches(phoneRegExp, {
+    message: 'Phone number must be valid',
+    excludeEmptyString: true
+  }),
+  debts: yup.array().of(
+    yup.object().shape({
+      debtType: yup
+        .mixed()
+        .oneOf([...debtTypes, unknown], 'Debt type is required'),
+      studentDebtType: yup
+        .mixed()
+        .oneOf([...studentDebtTypes, unknown], 'Student debt type is required'),
+      amount: yup.number().required('Amount is required'),
+      interestRate: yup.string().required('Interest rate is required'),
+      creditor: yup.string().required('Creditor is required'),
+      accountStatus: yup
+        .mixed()
+        .oneOf([...accountStatuses, unknown], 'Account status is required'),
+      beingHarrased: yup.string().required('You need to answer this question')
+    })
+  )
+})
 
 const Query = {}
 
@@ -10,6 +69,7 @@ const Mutation = {
     // get data from args
     const { data } = args
     const { user, campaign } = context
+    let errors
 
     // find data dues action for current campaign
     const [action] = await Action.query().where({
@@ -17,16 +77,21 @@ const Mutation = {
       type: 'data-dues'
     })
 
-    // validate data
-    // const errors = yup.validate(validationSchema, data)
+    try {
+      await validationSchema
+        .validate(data, { abortEarly: false })
+        .catch(validationErrors => {
+          errors = validationErrors.inner.map(err => {
+            return { field: err.path, message: err.message }
+          })
+        })
+    } catch (e) {}
 
-    // if invalid return errors
-
-    /* if (!_.isEmpty(errors)) {
+    if (!_.isEmpty(errors)) {
       return {
         errors
       }
-    } */
+    }
 
     const userAction = await UserAction.query().insert({
       userId: user.id,
