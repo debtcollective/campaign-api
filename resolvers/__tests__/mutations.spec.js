13df5f10810ba376c62e94ed49303000f2a1c859
Alexis Duran <alexis.duranm@gmail.com>
2019-11-27 10:45:16 +0100

    test(mutations): assert over motive value after user campaign link

--- resolvers/__tests__/mutations.spec.js
+++ resolvers/__tests__/mutations.spec.js
@@ -18,6 +18,7 @@ afterEach(async () => {
 describe('#addUserToCampaign', () => {
   it('links a user to a campaign if it doesnt have one yet', async () => {
     const responses = Array(2)
+    const originalMotive = 'already-on-strike'
     const userData = createUser()
     const campaignData = createCampaign()
     await User.query().insert(userData)
@@ -31,7 +32,7 @@ describe('#addUserToCampaign', () => {
 
     responses[0] = await mutationResolvers.addUserToCampaign(
       null,
-      { motive: 'already-on-strike' },
+      { motive: originalMotive },
       context
     )
     responses[1] = await mutationResolvers.addUserToCampaign(
@@ -47,5 +48,8 @@ describe('#addUserToCampaign', () => {
     expect(responses[0].ok).toBeTruthy()
     expect(responses[1].ok).toBeFalsy()
     expect(targetedUser.campaigns).toHaveLength(1)
+    expect(targetedUser.campaigns[0].data).toEqual(
+      expect.objectContaining({ motive: originalMotive })
+    )
   })
 })
